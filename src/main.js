b3762ca35da947cd3c3a09510d74f475b91b45ae
cxymds <cxymds@qq.com>
2021-05-31 16:02:01 +0800

    集成axios

--- src/main.js
+++ src/main.js
@@ -5,5 +5,6 @@ import router from './router'
 import store from './store'
 import Vant from 'vant';
 import 'vant/lib/index.css';
+import axios from './plugins/axios'
 
-createApp(App).use(store).use(router).use(Vant).mount('#app')
+createApp(App).use(store).use(router).use(Vant).use(axios).mount('#app')
--- src/plugins/axios.js
+++ src/plugins/axios.js
@@ -1,6 +1,6 @@
 "use strict";
 
-import Vue from 'vue';
+// import Vue from 'vue';
 import axios from "axios";
 
 // Full config:  https://github.com/axios/axios#request-config
@@ -39,23 +39,8 @@ _axios.interceptors.response.use(
   }
 );
 
-Plugin.install = function(Vue, options) {
-  Vue.axios = _axios;
-  window.axios = _axios;
-  Object.defineProperties(Vue.prototype, {
-    axios: {
-      get() {
-        return _axios;
-      }
-    },
-    $axios: {
-      get() {
-        return _axios;
-      }
-    },
-  });
-};
 
-Vue.use(Plugin)
+
+// Vue.use(Plugin)
 
 export default Plugin;
