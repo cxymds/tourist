b750f5542700f1d6d2d295451cd5c47685a79a23
cxymds <cxymds@qq.com>
2021-05-31 16:57:30 +0800

    集成axios

--- src/main.js
+++ src/main.js
@@ -1,10 +1,10 @@
-import './plugins/axios'
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import axios from 'axios'
+import VueAxios from 'vue-axios'
 import Vant from 'vant';
 import 'vant/lib/index.css';
-import axios from './plugins/axios'
 
-createApp(App).use(store).use(router).use(Vant).use(axios).mount('#app')
+createApp(App).use(store).use(router).use(Vant).use(VueAxios,axios).mount('#app')
