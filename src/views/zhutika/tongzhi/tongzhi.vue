39149894288e23812234de7df0b9d8e4a1287398
guoxiaofeng-gh <gxf18632339236@163.com>
2021-06-01 22:04:02 +0800

    主题咖 内 通知页面  加入 NavBar导航栏

---
+++ src/views/zhutika/tongzhi/tongzhi.vue
@@ -0,0 +1,22 @@
+<template>
+  <van-nav-bar
+    title="信息通知"
+    left-text="返回"
+    right-text="按钮"
+    left-arrow
+    @click-left="onClickLeft"
+    @click-right="onClickRight"
+  />
+</template>
+import { Toast } from 'vant';
+
+export default {
+  setup() {
+    const onClickLeft = () => Toast('返回');
+    const onClickRight = () => Toast('按钮');
+    return {
+      onClickLeft,
+      onClickRight,
+    };
+  },
+};
