361f4a3488f1f4cdb14467cd352af10b6968acac
ma20010428 <1990118782@qq.com>
2021-06-03 17:12:02 +0800

    登录完整

--- src/views/login.vue
+++ src/views/login.vue
@@ -6,25 +6,14 @@
     </div>
     <div class="form">
       <van-form @submit="onSubmit">
-      <van-field
-        v-model="user.phone"
-        name="手机号码"
-        label="手机号码"
-        placeholder="手机号码"
-        :rules="[{ required: true, message: '请填写手机号' }]"
-      />
+      <van-field v-model="user.phone" type="tel" label="手机号码" placeholder="手机号码" name="手机号码" :rules="[{ required: true, message: '请填写手机号' }]"/>
       <div class="yzm">
         <van-button type="info" @click="gain" class="yzmm">获取验证码</van-button>
       </div>
-      <van-field
-        v-model="user.code"
-        name="验证码"
-        label="验证码"
-        placeholder="验证码"
-        :rules="[{ required: true, message: '请填写验证码' }]"
-      />
+      <van-field v-model="user.code" type="digit" label="验证码" :rules="[{ required: true, message: '请填写验证码' }]" placeholder="验证码"  name="验证码"/>
       <div style="margin: 16px;">
         <van-button class="tijiao" block type="info" native-type="submit" @click="tijiao">提交</van-button>
+        <van-button class="tijiao" block type="info" native-type="submit" @click="jiao">跳转</van-button>
       </div>
     </van-form>
     </div>
@@ -36,8 +25,9 @@ export default {
     return {
       user:{
         phone: '',
-        code: '',
-      }
+        code: ''
+      },
+      shuju: ''
     };
   },
   methods: {
@@ -50,7 +40,21 @@ export default {
       })
     },
     tijiao(){
-       this.$router.push('/userlogin');
+      this.axios.post('/api/login',{mobile:this.user.phone,verify:this.user.code}).then((res)=>{
+        console.log(res)
+        var status = res.data.code;
+        console.log(status)
+        if (status == 200) {
+          this.shuju = res.data;
+          window.localStorage.setItem(
+            "token",
+            res.data.token
+          );
+        }
+      })
+    },
+    jiao(){
+      this.$router.push('/userlogin')
     }
   },
 };
