e2f3db1988277d7ac3a237ba4925cbd5c80217fa
guoxiaofeng-gh <gxf18632339236@163.com>
2021-06-03 09:26:30 +0800

    消息详情页面完成

--- src/views/zhutika/tongzhi/xiaoxixiangqing.vue
+++ src/views/zhutika/tongzhi/xiaoxixiangqing.vue
@@ -6,30 +6,58 @@
       left-arrow
       @click-left="onClickLeft"
     />
-    <div class="kuang">
-      <div class="xiaoxi"></div>
-      <div class="xiangqing"></div>
+    <div class="kuang" v-for="item in list" :key="item.id">
+      <div class="xiaoxi">
+        {{ item.xiaoxi }}
+      </div>
+      <div class="xiangqing" v-html="item.xiangqing"></div>
     </div>
   </div>
 </template>
+<script>
+export default {
+  name: "",
+  data() {
+    return {
+      list: [
+        {
+          xiaoxi:
+            "您有一笔预支付超额未及时处理，请于今天11:30之前往前台进行相关账单办理!",
+          xiangqing: `2020/12/30，周五，08:30 AM<br>
+          尊敬的游客，您好!你有一笔预支付超额必须在当天上午9:00前在前台结算，
+          如果您有任何疑问，请与我们联系，酒店内拨号“124“咨询。<br>
+          祝您游玩愉快!  `,
+        },
+      ],
+    };
+  },
+  methods: {},
+};
+</script>
+
+
 <style scoped>
-div {
-  border: 1px solid #000;
-}
 .kuang {
   width: 100%;
   height: 195px;
-  margin-top: 10px;
+  margin-top: 20px;
+  text-align: left;
+  font-size: 14px;
 }
 .xiaoxi {
   width: 375px;
   height: 40px;
   margin: 0 auto;
+  color: #1c222a;
 }
 .xiangqing {
+  box-sizing: border-box;
+  padding-top: 20px;
   width: 375px;
   height: 150px;
   margin: 0 auto;
+  color: #999999;
+  line-height: 22px;
 }
 </style>
 
