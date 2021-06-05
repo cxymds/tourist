daafc1fb27d623f7fa67c0e2e13510de2e3c12c5
ma20010428 <1990118782@qq.com>
2021-06-05 17:08:18 +0800

    距离完成只差一步之遥，很无奈

--- vue.config.js
+++ vue.config.js
@@ -1,14 +1,5 @@
 module.exports ={
   devServer: {
-    // overlay: { // 让浏览器 overlay 同时显示警告和错误
-    //   warnings: true,
-    //   errors: true
-    // },
-    // open: false, // 是否打开浏览器
-    // host: "localhost",
-    // port: "8080", // 代理断就
-    // https: false,
-    // hotOnly: false, // 热更新
     proxy: {
       "/api": {
         target:
@@ -22,6 +13,5 @@ module.exports ={
       }
     }
   },
-
   lintOnSave: false
 }
