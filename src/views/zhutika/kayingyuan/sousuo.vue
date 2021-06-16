dad5a1761a99b64fd0de38e19b0a2552e2336b1a
guoxiaofeng-gh <gxf18632339236@163.com>
2021-06-16 09:31:27 +0800

    sousuo

--- src/views/zhutika/kayingyuan/sousuo.vue
+++ src/views/zhutika/kayingyuan/sousuo.vue
@@ -32,53 +32,42 @@
         </div>
       </div>
       <div class="lishi_jilu">
-        <div class="lishi_jilu_ming">电影</div>
-        <van-sticky>
-          <van-button type="primary">12312</van-button>
-        </van-sticky>
+        <van-button
+          class="lishi_jilu_ming"
+          size="small"
+          v-for="(item, index) in lishi"
+          :key="index"
+          >{{ item.li }}</van-button
+        >
       </div>
     </div>
+    <!-- 热门搜索 -->
+    <div class="remen">热门搜索</div>
+
+    <van-list
+      v-model:loading="state.loading"
+      :finished="state.finished"
+      finished-text="没有更多了"
+      @load="onLoad"
+    >
+      <van-cell v-for="item in state.list" :key="item" :title="item" />
+    </van-list>
   </div>
 </template>
 
 <style scoped>
-.lishi {
+/* 热门搜索 */
+.remen {
+  box-sizing: border-box;
   width: 375px;
-  height: 100px;
-}
-.lishi_sousuo {
-  width: 375px;
-  height: 30px;
-  margin-top: 5px;
   border: 1px solid #000;
-}
-.lishi_sousuo_left {
-  margin-left: 15px;
-  float: left;
+  padding-left: 15px;
+  text-align: left;
+  margin-top: 40px;
   color: #999999;
-}
-.lishi_sousuo_right {
-  margin-right: 15px;
-  float: right;
-  color: #c6c9cc;
-}
-.lishi_sousuo_right_img {
-  position: relative;
-  top: 2px;
-  left: 0px;
+  font-weight: 600;
 }
 
-.lishi_jilu {
-  width: 375px;
-  height: 80px;
-  border: 1px solid #000;
-}
-.lishi_jilu_ming {
-  width: 150px;
-  border: 1px solid #000;
-  background-color: rgb(240, 240, 240);
-  color: #666666;
-}
 /* 搜索 */
 .sousuo {
   display: flex;
@@ -101,12 +90,94 @@
   border-bottom-right-radius: 25px;
 }
 /* 历史记录  */
+
+.lishi {
+  width: 375px;
+  height: 100px;
+}
+.lishi_sousuo {
+  width: 375px;
+  height: 30px;
+  margin-top: 5px;
+}
+.lishi_sousuo_left {
+  margin-left: 15px;
+  float: left;
+  color: #999999;
+}
+.lishi_sousuo_right {
+  margin-right: 15px;
+  float: right;
+  color: #c6c9cc;
+}
+.lishi_sousuo_right_img {
+  position: relative;
+  top: 2px;
+  left: 0px;
+}
+
+.lishi_jilu {
+  text-align: left;
+}
+.lishi_jilu_ming {
+  padding: 5px 10px;
+  margin: 5px 10px;
+  background-color: rgb(240, 240, 240);
+  color: #666666;
+  border-radius: 8px;
+}
 </style>
 
 <script>
+import { reactive } from "vue";
 export default {
+  setup() {
+    const state = reactive({
+      list: [],
+      loading: false,
+      finished: false,
+    });
+
+    const onLoad = () => {
+      // 异步更新数据
+      // setTimeout 仅做示例，真实场景中一般为 ajax 请求
+      setTimeout(() => {
+        for (let i = 0; i < 10; i++) {
+          state.list.push(state.list.length + 1);
+        }
+
+        // 加载状态结束
+        state.loading = false;
+
+        // 数据全部加载完成
+        if (state.list.length >= 40) {
+          state.finished = true;
+        }
+      }, 1000);
+    };
+
+    return {
+      state,
+      onLoad,
+    };
+  },
   data() {
-    return {};
+    return {
+      lishi: [
+        {
+          li: "哆啦A梦大电影",
+        },
+        {
+          li: "蜡笔小新",
+        },
+        {
+          li: "名侦探柯南",
+        },
+        {
+          li: "樱桃小丸子",
+        },
+      ],
+    };
   },
   methods: {
     fanhui() {
